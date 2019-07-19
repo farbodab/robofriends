dd3f48805bc97171588564edd0fc56c8cfbd6d14
Farbod Abolhassani <farbod.ab@hotmail.ca>
2019-07-19 14:15:30 -0400

    Add abstraction and move others into card

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card({id, name, email}) {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt="robots" src={`https://robohash.org/${id}?size=200x200`}></img>
       <div>
         <h2>{ name }</h2>
